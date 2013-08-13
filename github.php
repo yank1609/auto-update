e35efe60a2808b869921fb3c8261ed60be81e563
root <root@hms.(none)>
2013-08-13 01:48:46 -0400

    Revise Data

--- github.php
+++ github.php
@@ -1,1 +1,17 @@
-<?php exec('git pull'); ?>
+<?php 
+
+// Init vars
+$LOCAL_ROOT         = "/var/www/web/";
+$LOCAL_REPO_NAME    = "auto-update";
+$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+$REMOTE_REPO        = "git@github.com:yank1609/auto-update.git";
+$DESIRED_BRANCH     = "origin master";
+
+// Delete local repo if it exists
+
+// Clone fresh repo from github using desired local repo name and checkout the desired branch
+echo shell_exec("cd {$LOCAL_REPO} && git pull {$DESIRED_BRANCH}");
+
+die("done " . mktime());
+
+?>
