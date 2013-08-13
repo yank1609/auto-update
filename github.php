d7e34ad5a651ab251b0a1fec3716ebc2d457704e
root <root@hms.(none)>
2013-08-13 01:59:13 -0400

    revise

--- github.php
+++ github.php
@@ -1,17 +1,22 @@
 <?php 
+$date = strftime('%c');
+file_put_contents('log.txt', PHP_EOL.trim($date).PHP_EOL, FILE_APPEND);
+try
+{
+  $payload = json_decode($_REQUEST['payload']);
+}
+catch(Exception $e)
+{
+  exit(0);
+}
 
-// Init vars
-$LOCAL_ROOT         = "/var/www/web/";
-$LOCAL_REPO_NAME    = "auto-update";
-$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:yank1609/auto-update.git";
-$DESIRED_BRANCH     = "origin master";
+//LOG THE POST REQUEST
+file_put_contents('log.txt', print_r($payload, TRUE), FILE_APPEND);
 
-// Delete local repo if it exists
-
-// Clone fresh repo from github using desired local repo name and checkout the desired branch
-echo shell_exec("cd {$LOCAL_REPO} && git pull {$DESIRED_BRANCH}");
-
-die("done " . mktime());
+//EXECUTE A SCRIPT WHEN THE POST REQUEST IS INITIALIZED
+//if ($payload->ref === 'refs/heads/master')
+//{
+//  exec('deploy.sh >> log.txt');
+//}
 
 ?>
